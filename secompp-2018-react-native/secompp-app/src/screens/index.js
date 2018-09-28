8f4f8a8af8b29729a964c6bd546bd06376fdc574
Guilherme Vincenzi Cavichioli <guilhermecavichioli@hotmail.com>
2018-09-28 02:13:51 -0300

    Created some screens;

---
+++ secompp-2018-react-native/secompp-app/src/screens/index.js
@@ -0,0 +1,9 @@
+import FeedScreen from './FeedScreen';
+import LoginScreen from './LoginScreen';
+import HomeScreen from './HomeScreen';
+
+export {
+  FeedScreen,
+  LoginScreen,
+  HomeScreen,
+};
