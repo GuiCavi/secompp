2a5f68923f409c95649f8411c14e402e358070d0
Guilherme Vincenzi Cavichioli <guilhermecavichioli@hotmail.com>
2018-09-28 02:16:10 -0300

    Created Logo and LinearIcon components; Added Avenir fonts; Prevent app from loading before caching assets;

--- secompp-2018-react-native/secompp-app/App.js
+++ secompp-2018-react-native/secompp-app/App.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
+import { Font, AppLoading } from 'expo';
 
 import Router from './src/Router';
 
 export default class App extends Component {
+  state = {
+    isReady: false,
+  };
+
+  async cacheResourcesAsync() {
+    await Font.loadAsync({
+      'avenir-light': require('./assets/fonts/Avenir/Avenir-Light.ttf'),
+      'avenir-light-oblique': require('./assets/fonts/Avenir/Avenir-LightOblique.ttf'),
+
+      'avenir-book': require('./assets/fonts/Avenir/Avenir-Book.ttf'),
+      'avenir-book-oblique': require('./assets/fonts/Avenir/Avenir-BookOblique.ttf'),
+
+      'avenir-roman': require('./assets/fonts/Avenir/Avenir-Roman.ttf'),
+      'avenir-oblique': require('./assets/fonts/Avenir/Avenir-Oblique.ttf'),
+
+      'avenir-medium': require('./assets/fonts/Avenir/Avenir-Medium.ttf'),
+      'avenir-medium-oblique': require('./assets/fonts/Avenir/Avenir-MediumOblique.ttf'),
+
+      'avenir-heavy': require('./assets/fonts/Avenir/Avenir-Heavy.ttf'),
+      'avenir-heavy-oblique': require('./assets/fonts/Avenir/Avenir-HeavyOblique.ttf'),
+
+      'avenir-black': require('./assets/fonts/Avenir/Avenir-Black.ttf'),
+      'avenir-black-oblique': require('./assets/fonts/Avenir/Avenir-BlackOblique.ttf'),
+
+      'Linearicons-Free': require('./assets/fonts/LinearIcons/Linearicons-Free.ttf'),
+    });
+
+    return true;
+  }
+
   render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this.cacheResourcesAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+    
     return <Router />;
   }
 };
---
+++ secompp-2018-react-native/secompp-app/src/components/index.js
@@ -0,0 +1,7 @@
+import LinearIcon from './LinearIcon';
+import Logo from './Logo';
+
+export {
+  LinearIcon,
+  Logo,
+};
